041b4b23f63563f9d62c7dc875ba249c95bd8b9d
salahudeen abdulkareem <salayo4real@gmail.com>
2025-10-01 21:58:44 +0100

    alx-project-0x05 directory created

---
+++ alx-project-0x05/interface/index.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+
+
+
+export interface PageRouteProps {
+  pageRoute: string
+}
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
